5e7c88d747ea16e0af108369d2b8bdfdfeb82da9
Denis Parshin <goorujke@yandex.ru>
2021-08-18 23:34:10 +0300

    Спринт 4 Итерация 2.1

--- script/script.js
+++ script/script.js
@@ -2,38 +2,44 @@ let openPopupButton = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form')
-let formSubmitButton = document.querySelector('.popup__button')
 let nameInput = document.querySelector('.profile__title');
 let jobInput = document.querySelector('.profile__subtitle');
 let popupInputName = document.querySelector('.popup__field_name');
 let popupInputJobs = document.querySelector('.popup__field_jobs');
-let hearts = document.querySelectorAll('.element__heart');
+// это строка нужна для лайка. Будет использована в 5 спринте
+//let hearts = document.querySelectorAll('.element__heart');
 
 
-function togglePopup () {
-  popup.classList.toggle('popup_opened');
+function openPofilePopup () {
+  popup.classList.add('popup_opened');
   popupInputName.value = nameInput.innerText;
   popupInputJobs.value = jobInput.innerText;
 }
 
-openPopupButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
-
-
-function heartFillToggle(event) {
-   event.target.classList.toggle('element__heart_fill');
- }
-
-for (let i = 0; i <hearts.length; i++) {
-  hearts[i].addEventListener('click', heartFillToggle);
+function closePofilePopup () {
+  popup.classList.remove('popup_opened');
 }
+
+
+
+// Реализация постановки лайков. Использую в 5 спринте
+// function heartFillToggle(event) {
+//    event.target.classList.toggle('element__heart_fill');
+//  }
+
+// for (let i = 0; i <hearts.length; i++) {
+//   hearts[i].addEventListener('click', heartFillToggle);
+// }
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     nameInput.textContent=popupInputName.value;
     jobInput.textContent=popupInputJobs.value;
-    togglePopup ();
+    closePofilePopup ();
 }
- formElement.addEventListener('submit', formSubmitHandler);
+
+openPopupButton.addEventListener('click', openPofilePopup);
+closePopupButton.addEventListener('click', closePofilePopup);
+formElement.addEventListener('submit', formSubmitHandler);
 
