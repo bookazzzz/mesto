50864b7dcd337d5bcb5c23703391195f99a1fcf5
Denis Parshin <goorujke@yandex.ru>
2021-11-05 02:57:24 +0300

    Спринт 9 Итерация 1.06

--- src/script/constants.js
+++ src/script/constants.js
@@ -29,7 +29,7 @@ const openPopupAddCardButton = document.querySelector('.profile__add-card');
 const popupButtonList = document.querySelectorAll('.popup__button');
 const nameInputElement = document.querySelector('.popup__field_type_name');
 const jobInputElement = document.querySelector('.popup__field_type_jobs');
-const avatarInputElement = document.querySelector('.popup__field-input_avatar');
+const avatarInputElement = document.querySelector('.popup__field_input_avatar');
 const avatarEdit = document.querySelector('.profile__edit-avatar');
-const buttonDeliteСonfirmation = document.querySelector('.popup__submit-button_delete-confirmation');
+const buttonDeliteСonfirmation = document.querySelector('.popup__submit_delete-confirmation');
 
