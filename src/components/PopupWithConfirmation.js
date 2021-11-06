a01f19651e03216d060c304dee759e65eff738cc
Denis Parshin <goorujke@yandex.ru>
2021-11-06 18:12:38 +0300

    Спринт 9 Итерация 2.01

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,46 +1,22 @@
 import Popup from "../components/Popup.js";
-// export default class PopupWithConfirmation extends Popup {
-//   constructor({ popupSelector, formSelector }) {
-//     super(popupSelector)
-
-//     this._formElement = document.querySelector(formSelector)
-//     this._buttonDelete = document.querySelector('.popup_delete-cofirm').querySelector('.popup__submit-delete')
-//   }
-
-//   setSubmitHandler(submitForm) {
-
-//     this._submitForm = submitForm
-
-//   }
-
-//   setEventListeners() {
-
-//     super.setEventListeners()
-//     this._buttonDelete.addEventListener('click', (event) => {
-//       event.preventDefault()
-//       this._submitForm()
-//     })
-
-//   }
-// }
-export default class PopupWithFormSubmit extends Popup {
+export default class PopupWithConfirmation extends Popup {
   constructor({ popupSelector, formSelector }) {
     super(popupSelector)
 
-    this._formElement = document.querySelector(formSelector)
+    this._formElement = document.querySelector(`${popupSelector} ${formSelector}`);
   }
 
-  setSubmitHandler(handleSubmit) {
-    this._handleSubmit = handleSubmit
+  setSubmitHandler(submitForm) {
+    this._submitForm = submitForm
   }
 
   setEventListeners() {
     super.setEventListeners()
-
     this._formElement.addEventListener('submit', (event) => {
       event.preventDefault()
-
-      this._handleSubmit()
+      this._submitForm()
     })
   }
 }
+
+
